7158eeb148bd2c04b53d49689734dc782cdc8a91
Daniel Strong <danielstrong.ds@gmail.com>
2020-03-31 20:52:09 +0900

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+
+import React from "react";
+
+export default function Index() {
+
+  return (
+    <div>
+      <p>Hello</p>
+      <p>World</p>
+    </div>
+  );
+}
