1c0281abb0558188e5684dcb576e1584822473ab
unknown <danielstrong.ds@gmail.com>
2020-04-29 13:21:02 +0900

    initial PageHead commit

---
+++ components/Shared/PageHead.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+type PageHeadProps = {
+  title?: string;
+  description?: string;
+  image?: string;
+  imageAlt?: string;
+  twitterCard?: string;
+};
+
+export default function PageHead({
+  title,
+  description,
+  image,
+  imageAlt,
+  twitterCard,
+}: PageHeadProps) {
+  const { pathname } = useRouter();
+
+  const isBlogPost = pathname.startsWith("/blog/");
+
+  return (
+    <Head>
+      <title>{`${title ? title + " | " : ""}`} Daniel Strong</title>
+      <meta name="description" content={description} />
+      <link rel="canonical" href="https://www.danielstrong.tech/" />
+      <meta property="og:type" content={isBlogPost ? "article" : "website"} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta
+        property="og:url"
+        content={`https://www.danielstrong.tech${pathname}`}
+      />
+      <meta property="og:site_name" content="Daniel Strong Tech" />
+      <meta name="twitter:card" content={twitterCard || "summary"} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image:alt" content={imageAlt} />
+      <meta name="twitter:creator" content="@DaStrongWon" />
+    </Head>
+  );
+}
