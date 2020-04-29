901cec103ab40083fbd89d67eb3e40c738a69908
unknown <danielstrong.ds@gmail.com>
2020-04-29 16:31:48 +0900

    added PageHead

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import GradientContainer from "components/Shared/GradientContainer";
+import PageHead from "components/Shared/PageHead";
 
 export default function Index() {
   return (
     <div>
+      <PageHead title="Home" />
+
       <GradientContainer>
         <div>Hello</div>
         <div>Hello</div>
