76645ad7de51be176a6a9b01a053508555236087
Daniel Strong <danielstrong.ds@gmail.com>
2020-04-30 16:42:44 +0900

    initial about page commit

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PageHead from "components/Shared/PageHead";
+
+export default function About() {
+  return (
+    <>
+      <PageHead
+        title="About Me"
+        description="Get to know the man behind Daniel Strong Tech."
+      />
+      <div>
+        <p>About Page</p>
+      </div>
+    </>
+  );
+}
