7b6d166f476a809719d037d2c01fcbada9324585
unknown <danielstrong.ds@gmail.com>
2020-05-04 16:20:09 +0900

    initial Button styles

---
+++ components/Shared/_css/ButtonStyles.ts
@@ -0,0 +1,20 @@
+import { css } from "styled-components";
+
+export default css<{ primary: boolean }>`
+  border: 2px solid
+    ${props =>
+      props.primary ? props.theme.colors.blue : props.theme.colors.pink};
+  background-color: ${props =>
+    props.primary ? props.theme.colors.blue : props.theme.colors.pink + "00"};
+  color: ${props => (props.primary ? "white" : props.theme.colors.pink)};
+  font-size: 1.4rem;
+  line-height: 1.4rem;
+  border-radius: 25px;
+  height: 50px;
+  padding: 10px 20px;
+  cursor: pointer;
+  position: relative;
+  min-width: 100px;
+  display: flex;
+  justify-content: center;
+`;
