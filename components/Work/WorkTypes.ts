3d0065562974bbfa02887858882f0300db2cb26b
unknown <danielstrong.ds@gmail.com>
2020-05-12 16:16:31 +0900

    updated types

--- components/Work/WorkTypes.ts
+++ components/Work/WorkTypes.ts
@@ -2,7 +2,7 @@ export type WorkTypes = {
   site_name: string;
   image: string;
   description: string;
-  tech_used: string[];
+  tech_used?: string[];
   show_work: boolean;
   links: {
     github?: string;
