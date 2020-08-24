eb5b2c4e59574015ea82861ad0467913b9625c8a
unknown <danielstrong.ds@gmail.com>
2020-08-24 11:34:13 +0900

    moved blog page file to new blog folder

---
+++ pages/blog/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PageHead from "components/Shared/PageHead";
+
+export default function Blog() {
+  return (
+    <>
+      <PageHead
+        title="Blog"
+        description="Come and join me in learning new and useful things in web development."
+      />
+      <div>
+        <p>Blog Page</p>
+      </div>
+    </>
+  );
+}
