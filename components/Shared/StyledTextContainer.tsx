c1f43dfd5e00b258cd97d93d71a28e10d9747935
unknown <danielstrong.ds@gmail.com>
2020-10-07 10:22:30 +0900

    removed letter spacing and line-height

--- components/Shared/StyledTextContainer.tsx
+++ components/Shared/StyledTextContainer.tsx
@@ -3,8 +3,6 @@ import RemoveBookendStyles from "./_css/RemoveBookendStyles";
 
 export const StyledTextContainer = styled.div`
   font-size: 1.3rem;
-  line-height: 150%;
-  letter-spacing: 0.1px;
   color: ${props => props.theme.colors.text.dark};
   min-width: 300px;
   width: 100%;
@@ -17,10 +15,6 @@ export const StyledTextContainer = styled.div`
 
   ${props => props.theme.media.xl} {
     font-size: 1.1rem;
-  }
-
-  ${props => props.theme.media.md} {
-    letter-spacing: 0.2px;
   }
 
   ${props => props.theme.media.sm} {
