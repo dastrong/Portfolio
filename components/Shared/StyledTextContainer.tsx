0177cdac0ee0a5a9d0bc921db92a5ea78d116e7a
unknown <danielstrong.ds@gmail.com>
2020-10-07 13:37:14 +0900

    increased line height responsivity

--- components/Shared/StyledTextContainer.tsx
+++ components/Shared/StyledTextContainer.tsx
@@ -3,6 +3,7 @@ import RemoveBookendStyles from "./_css/RemoveBookendStyles";
 
 export const StyledTextContainer = styled.div`
   font-size: 1.3rem;
+  line-height: 1.8;
   color: ${props => props.theme.colors.text.dark};
   min-width: 300px;
   width: 100%;
@@ -11,13 +12,16 @@ export const StyledTextContainer = styled.div`
 
   ${props => props.theme.media.xxl} {
     font-size: 1.2rem;
+    line-height: 1.7;
   }
 
   ${props => props.theme.media.xl} {
     font-size: 1.1rem;
+    line-height: 1.6;
   }
 
   ${props => props.theme.media.sm} {
+    line-height: 1.5;
     font-size: 1rem;
   }
 `;
