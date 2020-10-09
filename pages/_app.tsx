c1d09eb2d2bee0d420438c412cb179488e1b3a2e
Daniel Strong <danielstrong.ds@gmail.com>
2020-10-09 17:05:05 +0900

    added intersection observer polyfill

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "styled-components";
 import { theme, darkColors, lightColors } from "../utils";
 import Layout from "components/Layout";
 import "focus-visible";
+import "intersection-observer";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [isDarkTheme, setTheme] = useState(true);
