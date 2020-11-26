d1430dc980e797f07c05a5721b9ce1c8caeb350b
unknown <danielstrong.ds@gmail.com>
2020-11-26 15:52:56 +0900

    added bundle analyzer to view bundle sizes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const withOptimizedImages = require("next-optimized-images");
 
-module.exports = withOptimizedImages({
-  webpack: function (config) {
-    config.module.rules.push({
-      test: [/\.md$/, /\.yml$/],
-      use: "raw-loader",
-    });
-    config.node = {
-      fs: "empty",
-    };
-    return config;
-  },
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
+
+module.exports = withBundleAnalyzer(
+  withOptimizedImages({
+    webpack: function (config) {
+      config.module.rules.push({
+        test: [/\.md$/, /\.yml$/],
+        use: "raw-loader",
+      });
+      config.node = {
+        fs: "empty",
+      };
+      return config;
+    },
+  })
+);
