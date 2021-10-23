1b5db46ddafc13881aec7a8a5bf3f6eb917218ae
Daniel Strong <danielstrong.ds@gmail.com>
2021-10-23 23:48:10 +0900

    updated packages and configuration

--- next.config.js
+++ next.config.js
@@ -1,24 +1,14 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const withOptimizedImages = require("next-optimized-images");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer(
-  withOptimizedImages({
-    // removes larger image being bundled currently
-    images: {
-      limit: 1000,
-    },
-    webpack: function (config) {
-      config.module.rules.push({
-        test: [/\.md$/, /\.yml$/],
-        use: "raw-loader",
-      });
-      config.node = {
-        fs: "empty",
-      };
-      return config;
-    },
-  })
-);
+module.exports = withBundleAnalyzer({
+  webpack: function (config) {
+    config.module.rules.push({
+      test: [/\.md$/, /\.yml$/],
+      use: "raw-loader",
+    });
+    return config;
+  },
+});
