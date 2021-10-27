983d869277875c6fb13c4de5d75c95c6df9580e5
Daniel Strong <danielstrong.ds@gmail.com>
2021-10-27 16:10:43 +0900

    setup next/image

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 
 module.exports = withBundleAnalyzer({
+  images: {
+    loader: "cloudinary",
+    path: "https://res.cloudinary.com/dastrong/image/upload",
+    domains: ["res.cloudinary.com"],
+    deviceSizes: [320, 475, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256],
+  },
   webpack: function (config) {
     config.module.rules.push({
       test: [/\.md$/, /\.yml$/],
