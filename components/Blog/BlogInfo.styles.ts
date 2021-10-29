26ee4c748bd53061c17cf97ab957ea094d40657c
Daniel Strong <danielstrong.ds@gmail.com>
2021-10-29 19:37:33 +0900

    updated blog avatar to use next/image

--- components/Blog/BlogInfo.styles.ts
+++ components/Blog/BlogInfo.styles.ts
@@ -1,4 +1,5 @@
-import styled, { css } from "styled-components";
+import { default as NextImage } from "next/image";
+import styled from "styled-components";
 
 export const Container = styled.div`
   margin-top: -0.5rem;
@@ -7,13 +8,16 @@ export const Container = styled.div`
   align-items: center;
 `;
 
-export const Image = css`
+export const ImageWrapper = styled.div`
   border-radius: 100%;
   border: 2px solid ${props => props.theme.colors.accent};
-  height: 45px;
-  width: 45px;
   margin: 0;
   margin-right: 1rem;
+  line-height: 0;
+`;
+
+export const Image = styled(NextImage)`
+  border-radius: 100%;
 `;
 
 export const Name = styled.h5`
--- components/Blog/BlogInfo.tsx
+++ components/Blog/BlogInfo.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-import styled from "styled-components";
-import { Img } from "react-optimized-image";
-import OptimizedImage, { HQstyles } from "components/Shared/OptimizedImage";
-import * as Styled from "./BlogInfo.styles";
 import { formatDate } from "utils";
-
-const StyledImage = styled(Img)`
-  ${HQstyles}
-`;
+import * as Styled from "./BlogInfo.styles";
 
 export default function BlogInfo({ date }: { date: string }) {
   return (
     <Styled.Container>
-      <OptimizedImage
-        imgFile="_blogavatar.jpg"
-        alt="daniel avatar"
-        containerStyles={Styled.Image}
-      >
-        <StyledImage src={require(`images/_blogavatar.jpg`)} sizes={[45]} />
-      </OptimizedImage>
+      <Styled.ImageWrapper>
+        <Styled.Image
+          src="Portfolio/_blogavatar.jpg"
+          alt="daniel avatar"
+          height={45}
+          width={45}
+        />
+      </Styled.ImageWrapper>
 
       <div>
         <Styled.Name>Daniel Strong</Styled.Name>
