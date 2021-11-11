0f9b9dfd06e6fdd8b500ac47b74b696be583bfb0
Daniel Strong <danielstrong.ds@gmail.com>
2021-11-12 00:28:04 +0900

    corrected date option types

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,4 +1,8 @@
-const options = { day: "numeric", month: "long", year: "numeric" };
+const options = {
+  day: "numeric" as const,
+  month: "long" as const,
+  year: "numeric" as const,
+};
 
 export const formatDate = (date: string | Date) => {
   return new Date(date).toLocaleDateString(undefined, options);
