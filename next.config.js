676b2805da1380ff08a87a4850a4f9cc6df9e8f0
Daniel Strong <danielstrong.ds@gmail.com>
2021-11-22 15:41:01 +0900

    removed very large device size

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ module.exports = withPlaiceholder(
       loader: "cloudinary",
       path: process.env.CLOUD_URL,
       domains: ["res.cloudinary.com"],
-      deviceSizes: [320, 475, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      deviceSizes: [320, 475, 640, 750, 828, 1080, 1200, 1920],
       imageSizes: [16, 32, 48, 64, 96, 128, 256],
     },
     webpack: function (config) {
