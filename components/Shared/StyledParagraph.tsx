a7f5083387ed774b55038eaef505d9a3880542d3
Daniel Strong <danielstrong.ds@gmail.com>
2021-11-30 18:37:18 +0900

    another attempt at fixing font boosting

--- components/Shared/StyledParagraph.tsx
+++ components/Shared/StyledParagraph.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledParagraph = styled.p`
   line-height: 1.5;
   margin-bottom: 0.5rem;
+  max-height: 9999999px;
 
   &:only-child,
   &:last-child {
